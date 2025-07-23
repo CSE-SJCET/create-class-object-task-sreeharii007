29a9804ae39c6e8b25cbc1c9f62f804fd8c1bb6d
sreeharii007 <sreeharipramod2028@cs.sjcetpalai.ac.in>
2025-07-23 20:44:24 +0530

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -14,9 +14,6 @@ class Student{
 public class Main {
       public static void main(String[] args) {
            Student student=new Student();
-           student.name="Sdcd";
-           student.rollNumber=34;
-           student.grade='A';
            student.displayDetails();
         // TODO: Create a Student object and call the method to display details
     }
